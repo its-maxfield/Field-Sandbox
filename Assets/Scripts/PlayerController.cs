b53a467e4d789ca9bd48809ac511187968f9dfe2
Maxfield Friedman <maxfield.friedman12@gmail.com>
2025-09-26 13:10:35 -0700

    Player rotation and look axis control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Private vars
     [SerializeField]
     private float pSpeed = 1.0f;
 
     [SerializeField]
     private float rSpeed = 45.0f;
 
-    //Private vars
+    [SerializeField]
+    bool bInvertLookAxis = true;
+    
+    [SerializeField]
+    Camera playerCam;
+
     GameObject playerBase;
+    int lookAxis = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +29,8 @@ public class PlayerController : MonoBehaviour
         }
 
         Cursor.lockState = CursorLockMode.Locked;
+
+        lookAxis = bInvertLookAxis ? -1 : 1;
     }
 
     // Update is called once per frame
@@ -31,13 +40,15 @@ public class PlayerController : MonoBehaviour
         float horzDis = Input.GetAxis("Horizontal");
         float vertDis = Input.GetAxis("Vertical");
 
-        Vector3 direction = (transform.forward * vertDis + transform.right * horzDis) * pSpeed;
+        Vector3 direction = (transform.forward * vertDis + transform.right * horzDis) * pSpeed * Time.deltaTime;
 
         Vector3 playerPos = playerBase.transform.position;
         playerPos += direction;
         playerBase.transform.position = playerPos;
 
         //Rotational movement
-        transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rSpeed);
+        playerBase.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * rSpeed);
+        
+        playerCam.transform.Rotate(new Vector3(lookAxis * Input.GetAxis("Mouse Y"), 0, 0) * Time.deltaTime * rSpeed);
     }
 }
